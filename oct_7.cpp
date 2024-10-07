11d719b06ee8f7a38acb7ea6d27e1919d3e19310
iconium21 <iconium21@hotmail.com>
2024-10-07 21:17:19 +0500

    OCT 7
    Monday OCT 7 2024.

---
+++ oct_7.cpp
@@ -0,0 +1,14 @@
+/* Computer H.W Class X Oct-7-2024 (MONDAY)
+*/
+#include <iostream>
+using namespace std;
+main()
+{
+ cout<<"\t\t\t Calendar for: October 2024\n";
+ cout<<"\t\tMon\t Tue\t Wed\t Thu\t Fri\t Sat\t Sun\n";
+ cout<<"\t\t---\t 1\t 2\t 3\t 4\t 5\t 6\n";
+ cout<<"\t\t7 \t 8\t 9\t 10\t 11\t 12\t 13\n";
+ cout<<"\t\t14 \t 15\t 16\t 17\t 18\t 19\t 20\n";
+ cout<<"\t\t21 \t 22\t 23\t 24\t 25\t 26\t 27\n";
+ cout<<"\t\t28 \t 29\t 30\t 31\t ---\t ---\t ---\n";
+}
